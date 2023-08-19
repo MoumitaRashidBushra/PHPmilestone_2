61bf58a835088a68c9b27ee823afbb9fd87b30b1
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-08-19 21:41:56 +0600

    basic php

---
+++ variables.php
@@ -0,0 +1,11 @@
+<!-- Variables in PHP are used to store data values. They start with a dollar sign ($) followed by a name. Variable names are case-sensitive and can consist of letters, numbers, and underscores. -->
+<?php
+
+$firstName = "Moumita Rashid ";
+$lastName = "Bushra";
+$fullName = $firstName . " " . $lastName;
+$age= 10;
+
+echo "Hello, $fullName!";
+echo "age, $age "
+?>
