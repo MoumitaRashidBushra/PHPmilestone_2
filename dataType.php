a2eaa63267bc03b39a0ee5934980037210815b77
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-08-19 21:53:10 +0600

    variable of php

---
+++ dataType.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Moumita";      // String
+$age = 10;            // Integer
+$price = 99.99;       // Float
+$isboolean = true;    // Boolean
+$colors = ["red", "green", "blue"];  // Array
+
+?>
--- variables.php
+++ variables.php
@@ -6,6 +6,37 @@ $lastName = "Bushra";
 $fullName = $firstName . " " . $lastName;
 $age= 10;
 
-echo "Hello, $fullName!";
+$number1 = 100;
+$number2 = 500;
+
+$sum = $number1 + $number2;
+$result = $number1 * $number2;
+
+$language = "PHP";
+$version = 7;
+
+$message = "I am using $language version $version.";
+
+$globalVar = "I'm a global variable";
+
+function demoFunction() {
+    $localVar = "I'm a local variable";
+    echo $localVar;  // This works
+    echo $globalVar; // This won't work
+}
+
+demoFunction();
+
+echo $globalVar; // This works
+
+echo $message;
+
+echo "<br>Sum: $sum<br>";
+echo "Result: $result <br>";
+
+echo "Hello, $fullName!<br>";
 echo "age, $age "
+
+
+
 ?>
