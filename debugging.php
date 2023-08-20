d2d44bea270643e6afd15e63ae833f9b7159c9de
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-08-20 23:17:27 +0600

    Debugging

---
+++ debugging.php
@@ -0,0 +1,8 @@
+<?php
+
+//Debugging is the process of finding and fixing errors in your code. PHP offers various tools and techniques for debugging, including using echo statements, var_dump(), and error reporting.
+
+$age = 25;
+var_dump($age);  // Outputs int(25)
+
+?>
