ebebdacff822f1ccfe725c2a6c6e1d58faf47ee0
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-08-20 23:14:15 +0600

    cookies

---
+++ cookies.php
@@ -0,0 +1,7 @@
+<!-- Cookies are small pieces of data stored on the user's computer. They can be used to remember user preferences or other information. -->
+
+<?php
+
+setcookie("user", "alice", time() + 3600, "/");
+
+?>
